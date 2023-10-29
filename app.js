0ee83dc3c1cfbf82c2452b756a40d589beb556fb
Ngô Hồ Minh Quân <109454816+QuanThiccTech@users.noreply.github.com>
2023-10-29 10:28:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, updateDoc, doc, getDoc} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, updateDoc, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBWPCV6rYquQmpWa_TaLXr-YRKYjsXyXqM",
@@ -10,16 +10,14 @@ const firebaseConfig = {
   appId: "1:316672678531:web:4c8421026df75336e619b6"
 };
 
-
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 async function updateTodo(id) {
-  todoList.innerHTML = ''
-  const docRef = doc(db, "todo", id)
+  todoList.innerHTML = '';
+  const docRef = doc(db, "todo", id);
   const docSnap = await getDoc(docRef);
-  if (docSnap.data().check == "False"){
+  if (docSnap.data().check == "False") {
     await updateDoc(docRef, {
       check: "True"
     });
@@ -28,53 +26,33 @@ async function updateTodo(id) {
       check: "False"
     });
   }
+}
 
-async function deleteTodo(id){
-  localStorage.setItem("id delete", id)
+async function deleteTodo(id) {
   todoList.innerHTML = '';
-  await deleteDoc(doc(db, "todo", localStorage.getItem("id delete")));
+  await deleteDoc(doc(db, "todo", id));
+}
 
-};
+const todoList = document.getElementById("todo-list");
 
-  const querySnapshot = await getDocs(collection(db, "todo"));
-  querySnapshot.forEach((doc) => {
-  let todoOutput = document.createElement('li');
+const querySnapshot = await getDocs(collection(db, "todo"));
+querySnapshot.forEach((doc) => {
+  const todoOutput = document.createElement('li');
   todoOutput.innerHTML = `
     <p class="p-container"><input type="checkbox" id="checkbox-${doc.id}"> ${doc.data().id}</p> <button id="delete-${doc.id}">Delete</button>
-    `
+  `;
   todoList.appendChild(todoOutput);
   if (doc.data().check == "False") {
     document.getElementById(`checkbox-${doc.id}`).checked = false;
   } else {
     document.getElementById(`checkbox-${doc.id}`).checked = true;
   }
-  document.getElementById(`checkbox-${doc.id}`).addEventListener("change", () => updateTodo(doc.id))
-  document.getElementById(`delete-${doc.id}`).addEventListener("click", () => deleteTodo(doc.id))
-}); 
-}
+  document.getElementById(`checkbox-${doc.id}`).addEventListener("change", () => updateTodo(doc.id));
+  document.getElementById(`delete-${doc.id}`).addEventListener("click", () => deleteTodo(doc.id));
+});
 
-let todoList = document.getElementById("todo-list");
-
-const querySnapshot = await getDocs(collection(db, "todo"));
-querySnapshot.forEach((doc) => {
-let todoOutput = document.createElement('li');
-todoOutput.innerHTML = `
-  <p class="p-container"><input type="checkbox" id="checkbox-${doc.id}"> ${doc.data().id}</p> <button id="delete-${doc.id}">Delete</button>
-  `
-todoList.appendChild(todoOutput);
-if (doc.data().check == "False") {
-  document.getElementById(`checkbox-${doc.id}`).checked = false;
-} else {
-  document.getElementById(`checkbox-${doc.id}`).checked = true;
-}
-document.getElementById(`checkbox-${doc.id}`).addEventListener("change", () => updateTodo(doc.id))
-document.getElementById(`delete-${doc.id}`).addEventListener("click", () => deleteTodo(doc.id))
-}); 
-
-
-
-let todoInput = document.getElementById("todo-input")
-document.getElementById("todo-submit").onclick = async() => {
+const todoInput = document.getElementById("todo-input");
+document.getElementById("todo-submit").onclick = async () => {
   const docRef = await addDoc(collection(db, "todo"), {
     id: todoInput.value,
     check: "False"
@@ -83,21 +61,18 @@ document.getElementById("todo-submit").onclick = async() => {
   todoList.innerHTML = '';
   todoInput.value = '';
   const querySnapshot = await getDocs(collection(db, "todo"));
-querySnapshot.forEach((doc) => {
-  let todoOutput = document.createElement('li');
-  todoOutput.innerHTML = `
-    <p class="p-container"><input type="checkbox" id="${doc.data().value}"> ${doc.data().id}</p>  <button id="delete-${doc.id}">Delete</button>
-    `
-  todoList.appendChild(todoOutput);
-  if (doc.data().check == "False") {
-    document.getElementById(doc.value).checked = false;
-  } else {
-    document.getElementById(doc.value).checked = true;
-  }
-  document.getElementById(`checkbox-${doc.id}`).addEventListener("change", () => updateTodo(doc.id))
-  document.getElementById(`delete-${doc.id}`).addEventListener("click", () => deleteTodo(doc.id))
-
-}); 
-
-}
-
+  querySnapshot.forEach((doc) => {
+    const todoOutput = document.createElement('li');
+    todoOutput.innerHTML = `
+      <p class="p-container"><input type="checkbox" id="checkbox-${doc.id}"> ${doc.data().id}</p>  <button id="delete-${doc.id}">Delete</button>
+    `;
+    todoList.appendChild(todoOutput);
+    if (doc.data().check == "False") {
+      document.getElementById(`checkbox-${doc.id}`).checked = false;
+    } else {
+      document.getElementById(`checkbox-${doc.id}`).checked = true;
+    }
+    document.getElementById(`checkbox-${doc.id}`).addEventListener("change", () => updateTodo(doc.id));
+    document.getElementById(`delete-${doc.id}`).addEventListener("click", () => deleteTodo(doc.id));
+  });
+};
